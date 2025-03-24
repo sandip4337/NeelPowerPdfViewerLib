9e361283ef48a6dfacfada02e2aa1b1b291065ed
Sandip Saha <sandipsaha.neel99@gmail.com>
2025-03-24 19:13:53 +0530

    Add functionlity
    Add delete and get pdf path

--- PowerPdfLibrary/src/main/java/com/example/powerpdflibrary/PowerPdfLibChecker.kt
+++ PowerPdfLibrary/src/main/java/com/example/powerpdflibrary/PowerPdfLibChecker.kt
@@ -8,11 +8,39 @@ class PowerPdfLibChecker {
     companion object {
 
         private lateinit var filesDirPath: String
+        private lateinit var base46FilePath: String
 
         fun checkPdfExits(context: Context, pdfName: String, pdfId: String): Boolean {
             filesDirPath = context.filesDir.absolutePath
             val base46FilePath = "$filesDirPath/base64_$pdfName$pdfId.txt"
             return File(base46FilePath).exists()
+        }
+
+        fun getFilesDirPathAsFile(context: Context, pdfName: String, pdfId: String): File {
+            filesDirPath = context.filesDir.absolutePath
+            if (File("$filesDirPath/base64_$pdfName$pdfId.txt").exists()) {
+                base46FilePath = "$filesDirPath/base64_$pdfName$pdfId.txt"
+            }
+            return File(base46FilePath)
+        }
+
+        fun getFilesDirPathAsString(context: Context, pdfName: String, pdfId: String): String {
+            filesDirPath = context.filesDir.absolutePath
+            if (File("$filesDirPath/base64_$pdfName$pdfId.txt").exists()) {
+                base46FilePath = "$filesDirPath/base64_$pdfName$pdfId.txt"
+            }
+            return base46FilePath
+        }
+
+        fun deletePdfFile(context: Context, pdfName: String, pdfId: String) {
+            filesDirPath = context.filesDir.absolutePath
+            val base46FilePath = "$filesDirPath/base64_$pdfName$pdfId.txt"
+            val file = File(base46FilePath)
+            if (file.exists()) {
+                file.delete()
+            } else {
+                println("File not found: $base46FilePath")
+            }
         }
     }
 }
